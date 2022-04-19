256de37b09d580a6b9e1a4f609bb39a1e271a624
LПΣX <67869168+LNEX-ESP@users.noreply.github.com>
2022-04-19 23:21:06 +0200

    Add files via upload
    Primer Script en este veremos los comandos más básicos como echo (Para imprimir texto) y read para leer información, en este el Script nos preguntara nuestro nombre y edad y después imprimirá un texto con dicha información, tambien veremos algunos argumentos que puede recibir un Script como: número de argumentos que recibe el Script,  cadena de texto con todos los argumentos, nombre del Script y representar cada uno de los argumentos pasados al Script

---
+++ Script-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+echo "Vamos a conocerte y vender luego tus datos"
+read -p "¿Cual es tu Nombre? " NOMBRE
+read -p "¿Cual es tu Edad? " EDAD
+echo "Tu Nombre es $NOMBRE y tu edad es de $EDAD años
+
+------------------------------------------------
+Número de argumentos que recibe = $# 
+Nombre del script que se está ejecutando = $0
+Todos los argumentos que se le han pasado = $*
+------------------------------------------------"
+# LПΣX
