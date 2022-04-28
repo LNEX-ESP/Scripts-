c4c6547a3046036506ac945ddcaae0b97b666c0b
LПΣX <67869168+LNEX-ESP@users.noreply.github.com>
2022-04-28 10:07:12 +0200

    Add files via upload
    Saliéndonos un poco de la línea de progresión que llevábamos os doy un script de la más útil, con él podremos crearnos directorios de trabajo automáticamente, en este caso creamos un directorio de trabajo para una página web en donde le damos el nombre al directorio a través de un parámetro.

---
+++ creacion_directorios.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+if [ $# = 1  ]; then
+    cp -r "dirección del directorio ej /home/LПΣX/...." ./$pwd
+    mv ./$pwd/"nombr de la carpeta al cambiar el nombre" $1
+    echo "OK"
+else
+    echo "Introduce el Nombre de la carpeta"
+fi
+# LПΣX
